a7f2dcdc5b63db83589f70d3dccd844618238efa
Thiagojj <thiago.mello2412@gmail.com>
2022-05-12 14:50:44 -0300

    adicionando exercicio de const e let

---
+++ Curso/aula8/exercicio.js
@@ -0,0 +1,13 @@
+
+
+const nome = 'luiz otávio';
+const sobrenome = 'Miranda';
+const idade = 30;
+const peso = 84;
+const altura = 1.80;
+let imc = peso / (altura * altura);
+let anoNascimento = 2010 - 30;
+
+console.log(nome , sobrenome ,'tem', idade ,'anos, pesa', peso, 'kg tem', altura, 'e sei IMC é de', imc, nome, 'nasceu em', anoNascimento );
+
+
