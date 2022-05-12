e3ff9598e37c4476124dec73cbbcb2c1f4476bbb
Thiagojj <thiago.mello2412@gmail.com>
2022-05-12 17:54:01 -0300

    adicionando função abcd

---
+++ Curso/Aula1/transformar.js
@@ -0,0 +1,15 @@
+
+let str = 'abcdef';
+
+function transformar(letras) {
+    var result = [];
+    result.push(letras.substr(2,2))
+    console.log(result)
+    
+}
+
+transformar('abcdef')
+
+
+// console.log(str.substr(-20, 2).());
+// console.log(str.substr(2, 2).split());
