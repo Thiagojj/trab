ba11dd9e6a973cc3569e4fb2b678d2c4331a29dc
Thiagojj <thiago.mello2412@gmail.com>
2022-05-12 23:16:53 -0300

    adicionando codigo do stackoverflow

--- Curso/Aula1/array.js
+++ Curso/Aula1/array.js
@@ -1,9 +1,33 @@
 
-const letras = 'abcd'
+
+
+
+
+
+
+
+function transformar(letras) {
+    var novoArray = [];
+    var corte = 2;
+ if(letras.length % 2 != 0 )  {
+    letras = letras.concat('_')
+    // console.log(letras)
+
+ } 
+ for (var i = 0; i < letras.length; i = i + corte) {
+    novoArray.push(letras.slice(i, i + corte));
+  }
+
+
+    console.log(novoArray)
+//console.log(letrasTamanho)
+    
+}
+
+transformar('abcdefgh')
+
+/*const letras = 'abcd'
 console.log (letras.slice (0,2));
-console.log (letras.slice (2,4));
+console.log (letras.slice (2,4));*/
 
 
-/*const teste = ['thiago', 'joao' , 'maria'];
-
-console.log (teste[-1]);*/
