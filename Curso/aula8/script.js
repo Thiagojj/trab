f53adb189edeb015a4320d3dc66140507e8e98d6
Thiagojj <thiago.mello2412@gmail.com>
2022-05-20 11:18:53 -0300

    resolvendo o exercicio de operação com js

--- Curso/aula8/script.js
+++ Curso/aula8/script.js
@@ -1,7 +1,10 @@
 //alert('testando');
 
 //prompt('');
-
+// parseInt transforma string em inteiro;
+// parseFloat transforma string em representação decimal;
 let dados = prompt('');
+let dados2 = prompt('');
+let resultado = parseInt(dados) + parseInt(dados2);
 
-alert(dados);
+alert(`O resultado da conta foi ${resultado}`);
