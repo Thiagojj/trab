f5f6470e3d6925daaad183a506e1fcdec19196b4
Thiagojj <thiago.mello2412@gmail.com>
2022-05-29 21:26:56 -0300

    subindo exercecicio pela metade

---
+++ Curso/aula13/exercicio.js
@@ -0,0 +1,10 @@
+const nome = prompt('Digite seu nome completo:');
+  document.body.innerHTML += `Seu nome é: ${nome}<br />`;
+  document.body.innerHTML += `Seu nome tem ${nome.length} letras <br />`;
+  document.body.innerHTML += `A segunda letra do seu nome é:${nome.slice(1 , 2)}<br />`;
+  document.body.innerHTML += `Qual o primeiro índice da letra LETRA no seu nome? ${nome.search(/t/)}<br />`;
+  document.body.innerHTML += `Qual o último índice da letra LETRA no seu nome? ${nome.search(/o/i)}<br />`;
+  document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3)}<br />`;
+  document.body.innerHTML += `As palavras do seu nome são: ______<br />`;
+  document.body.innerHTML += `Seu nome com letras maiúsculas: ______<br />`;
+  document.body.innerHTML += `Seu nome com letras minúsculas: ______<br />`;
---
+++ Curso/aula13/variaveis.js
@@ -0,0 +1,29 @@
+//variavelde busca por caractere da string, seja ele ou letra, retorando a posição
+/*let UmaString = "Um texto"; 
+
+console.log(UmaString.search(/x/))*/
+//variavel para tamanho
+/*let UmaString = "Um texto"; 
+
+console.log(UmaString.length)*/
+
+//variavel para busca por determinado tamanho, se usar numero negativo começa de tras pra frente
+/*let UmaString = "Um texto"; 
+
+console.log(UmaString.slice(0, 4))*/
+
+//variavel que determina o que remover em caractere da string limitando a quatidade de vezes que acontece
+/*let UmaString = "Um texto aleatorio demais"; 
+
+console.log(UmaString.split('e', 2))*/
+
+//variavel que colocar o texto em minusculo ou em maiusculo
+let UmaString = "Um texto aleatorio demais"; 
+
+console.log(UmaString.toLowerCase());
+console.log(UmaString.toUpperCase());
+
+
+
+
+
